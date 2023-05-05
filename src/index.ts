91493690cadb2a44c944735bb54eaff9f2ea68f5
Isidor Nikolic <inikolic@microsoft.com>
2023-05-05 18:42:52 +0000

    add .well-known folder

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import swaggerUi from 'swagger-ui-express';
 import * as OpenApiValidator from 'express-openapi-validator';
 import cors from 'cors';
 import { Configuration, OpenAIApi } from "openai";
-const openApiSpecification = require('../src/openapi.json');
+const openApiSpecification = require('../.well-known/openapi.json');
 
 export default express;
 const app = express();
@@ -15,10 +15,10 @@ const configuration = new Configuration({
 	apiKey: process.env.OPENAI_API_KEY
 });
 const openai = new OpenAIApi(configuration);
-  
+app.use('/.well-known', express.static('.well-known'));
 app.get('/shows', async (_req: Request, res: Response) => {
 	const params = new URLSearchParams({
-		limit: '10'
+		limit: '8'
 	});
 	const response = await fetch(`${process.env.PODCAST_URL}shows?${params}`); 
 	const json = await response.json();
@@ -61,9 +61,10 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(openApiSpecification));
 app.use(
 	OpenApiValidator.middleware({
-		apiSpec: './src/openapi.json',
+		apiSpec: './.well-known/openapi.json',
 		validateRequests: true,
 		validateResponses: true,
+		ignorePaths: /\/\.well-known\// // Add this line to ignore the .well-known folder
 	}),
 );
 
