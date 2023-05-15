49882ecd511424b9afb68be22e18b76267119d8b
Isidor Nikolic <inikolic@microsoft.com>
2023-05-15 08:59:08 +0000

    episodes/:id/summary use .then style and use spaces for indent

--- src/index.ts
+++ src/index.ts
@@ -12,65 +12,65 @@ app.use(cors());
 dotenv.config();
 
 interface Show {
-	id: string;
-	title: string;
-	description: string;
-	episodes: Episode[];
-	image: string;
+    id: string;
+    title: string;
+    description: string;
+    episodes: Episode[];
+    image: string;
 }
 
 interface Episode {
-	description: string;
-	id: string;
-	title: string;
-	published: string;
+    description: string;
+    id: string;
+    title: string;
+    published: string;
 }
 
 app.use('/.well-known', express.static('.well-known'));
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(openApiSpecification));
 
 app.get('/shows', async (_req: Request, res: Response) => {
-	const params = new URLSearchParams({
-		limit: '6',
-		categoryId
-	});
-	const response = await fetch(`${process.env.PODCAST_URL}shows?${params}`); 
-	const json = await response.json();
-	res.send(json);
+    const params = new URLSearchParams({
+        limit: '6',
+        categoryId
+    });
+    const response = await fetch(`${process.env.PODCAST_URL}shows?${params}`); 
+    const json = await response.json();
+    res.send(json);
 });
 
 app.get('/shows/:id', async (req: Request, res: Response) => {
-	const response = await fetch(`${process.env.PODCAST_URL}shows/${req.params.id}`);
-	const json: Show = await response.json();
-	res.send(json);
+    const response = await fetch(`${process.env.PODCAST_URL}shows/${req.params.id}`);
+    const json: Show = await response.json();
+    res.send(json);
 });
 
-app.get('/episodes/:id/summary', async (req: Request, res: Response) => {
-	const response = await fetch(`${process.env.PODCAST_URL}episodes/${req.params.id}`);
-	let summary = '';
-	try {
-		const episode: Episode = await response.json();
-		summary = episode.description;
-	} catch (error) {
-		console.log(error);
-	}
-	res.send({ summary });
+app.get('/episodes/:id/summary', (req: Request, res: Response) => {
+    fetch(`${process.env.PODCAST_URL}episodes/${req.params.id}`).then((response) => {
+        response.json().then((json: Episode) => {
+            const summary = json.description;
+            res.send({ summary });
+        });
+    }, (error) => { 
+        console.log(error);
+        res.status(500).send({ error });
+    });
 });
 
 app.get('/', (_req: Request, res: Response) => {
-	res.send('Friendly Podcast Server is running!');
+    res.send('Friendly Podcast Server is running!');
 });
 
 app.use(
-	OpenApiValidator.middleware({
-		apiSpec: './.well-known/openapi.json',
-		validateRequests: true,
-		validateResponses: true,
-		ignorePaths: /\/\.well-known\// // Add this line to ignore the .well-known folder
-	}),
+    OpenApiValidator.middleware({
+        apiSpec: './.well-known/openapi.json',
+        validateRequests: true,
+        validateResponses: true,
+        ignorePaths: /\/\.well-known\// // Add this line to ignore the .well-known folder
+    }),
 );
 
 const port = 8080;
 app.listen(port, () => {
-	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
